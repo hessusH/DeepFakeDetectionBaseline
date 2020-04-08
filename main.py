87789059623f7f7367fcb462f2fed13308e4a4ab
Durinov <MDurinov@ad.harman.com>
2020-04-08 11:59:51 +0300

    minor updates in main.py, utils/storage.py was added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
+from torch.nn import DataParallel
 import argparse
 import yaml
 from models import classificators
+from utils.storage import load_weights, save_weights
 import numpy as np
 import torch.nn.functional as F
 
@@ -12,13 +13,16 @@ from torch.utils.tensorboard import SummaryWriter
 import datetime
 
 
+
+
+
 def train(model, optimizer, scheduler, criterion, train_loader, epoch, writer, config, device):
     model.train()
     model.to(device)
     for i, (image, label) in enumerate(train_loader):
         optimizer.zero_grad()
-        out = net(x)
-        loss = criterion(out, y)
+        out = model(image)
+        loss = criterion(out, label)
         loss.backwarconfigd()
         #             avg.append(loss.item())
         optimizer.step()
@@ -32,13 +36,32 @@ def train(model, optimizer, scheduler, criterion, train_loader, epoch, writer, c
 
 
 def main(config):
+    device = torch.device('cuda:0' if config['device'] == 'gpu' and torch.cuda.is_available() else 'cpu')
+
+
+    if config['snapshot']['use']:
+        model = getattr(classificators, config['model_name'])(config['num_classes'])
+        model.to(device)
+        load_weights(model, config['prefix'], config['model']['name'], config['snapshot']['epoch'])
+    else:
+        model = getattr(classificators, config['model_name'])(config['num_classes'])
+        model.to(device)
+
+    if torch.cuda.is_available() and config['parallel']:
+        model = DataParallel(model)
+
     date = datetime.datetime.now()
     writer = SummaryWriter(log_dir=str(date) + '_' + config['model_name'])
-    model = getattr(classificators, config['model_name'])(config['num_classes'])
+
+
     writer.add_graph(model)
-    criterion = nn.CrossEntropyLoss()
+    criterion = getattr(nn, config['criterion'])()
     optimizer = getattr(optim, config['optimizer'])(model.parameters(), lr=config['learning_rate'])
-    scheduler = getattr(optim.lr_scheduler, config['lr_scheduler'])(optimizer, config['step'])
+    # scheduler = getattr(optim.lr_scheduler, config['lr_scheduler'])(optimizer, config['step'])
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                     factor=config['learning_rate']['decay'],
+                                                     patience=config['learning_rate']['no_improve'],
+                                                     min_lr=config['learning_rate']['min_val'])
     train_loader = None
     test_loader = None
     val_loader = None
